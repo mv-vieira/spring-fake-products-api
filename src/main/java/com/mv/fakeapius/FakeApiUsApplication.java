266ec5897ef8f00edae48ded33f91ebb030840ba
Matheus Vicente <mv_vieira96@hotmail.com>
2024-05-10 20:39:43 -0300

    :wrench: chore - setting FeignClient

--- src/main/java/com/mv/fakeapius/FakeApiUsApplication.java
+++ src/main/java/com/mv/fakeapius/FakeApiUsApplication.java
@@ -2,8 +2,10 @@ package com.mv.fakeapius;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class FakeApiUsApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/mv/fakeapius/infrastructure/FakeApiClient.java
@@ -0,0 +1,7 @@
+package com.mv.fakeapius.infrastructure;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(value = "fake-api", url = "${fake-api.url:#(null)}")
+public interface FakeApiClient {
+}
