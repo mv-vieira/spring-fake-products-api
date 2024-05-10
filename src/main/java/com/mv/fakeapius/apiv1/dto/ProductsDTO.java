4f404b5a8958d34e94c2bda8a11f10b16ee0b980
Matheus Vicente <mv_vieira96@hotmail.com>
2024-05-10 20:45:09 -0300

    :sparkles: feat - creating ProductsDTO

---
+++ src/main/java/com/mv/fakeapius/apiv1/dto/ProductsDTO.java
@@ -0,0 +1,13 @@
+package com.mv.fakeapius.apiv1.dto;
+
+import java.math.BigDecimal;
+
+public class ProductsDTO {
+
+    private Long id;
+    private String title;
+    private BigDecimal price;
+    private String category;
+    private String description;
+    private String image;
+}
