2ddb25a7311ed72b6363444f8b0e84cf7725f9fc
Matheus Vicente <mv_vieira96@hotmail.com>
2024-05-10 20:58:26 -0300

    :sparkles: feat - implements method in interface to search all products

--- src/main/java/com/mv/fakeapius/infrastructure/FakeApiClient.java
+++ src/main/java/com/mv/fakeapius/infrastructure/FakeApiClient.java
@@ -1,7 +1,14 @@
 package com.mv.fakeapius.infrastructure;
 
+import com.mv.fakeapius.apiv1.dto.ProductsDTO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @FeignClient(value = "fake-api", url = "${fake-api.url:#(null)}")
 public interface FakeApiClient {
+
+    @GetMapping("/products")
+    List<ProductsDTO> findAllProducts();
 }
