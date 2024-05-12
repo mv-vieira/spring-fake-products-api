8f00b59ee66989c616feecc1db37d968137097b3
Matheus Vicente <mv_vieira96@hotmail.com>
2024-05-12 00:22:26 -0300

    :bug: fix - request error 500 was resolved

--- src/main/java/com/mv/fakeapius/apiv1/dto/ProductsDTO.java
+++ src/main/java/com/mv/fakeapius/apiv1/dto/ProductsDTO.java
@@ -1,13 +1,21 @@
 package com.mv.fakeapius.apiv1.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.math.BigDecimal;
 
 public class ProductsDTO {
 
+    @JsonProperty(value = "id")
     private Long id;
+    @JsonProperty(value = "title")
     private String title;
+    @JsonProperty(value = "price")
     private BigDecimal price;
+    @JsonProperty(value = "category")
     private String category;
+    @JsonProperty(value = "description")
     private String description;
+    @JsonProperty(value = "image")
     private String image;
 }
